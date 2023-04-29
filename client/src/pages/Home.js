05dee418925126e000b529031c805508eb31e19e
Sander Weiss <weisssander@gmail.com>
2023-04-29 15:52:36 -0400

    implementing pages and components so things render

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+const Home = () => {
+    return (
+        <main>
+            <div>
+                <h1>Home Placeholder</h1>
+            </div>
+        </main>
+    )
+};
+
+export default Home;

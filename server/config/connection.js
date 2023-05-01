8154e970c985f6b852677fc642f7975d017af918
ragingpenguin <eflinjh@gmail.com>
2023-05-01 18:12:04 -0400

    added _db to connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/groceryjad');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/groceryjad_db');
 
 module.exports = mongoose.connection;

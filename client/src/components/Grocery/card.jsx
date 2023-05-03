2b13afcb59f9549ba4bcc570de81017b81d63b78
ragingpenguin <eflinjh@gmail.com>
2023-05-03 16:48:17 -0400

    adding cards

---
+++ client/src/components/Grocery/card.jsx
@@ -0,0 +1,29 @@
+
+
+export function RecipeCard() {
+
+
+
+    return (
+        <>
+        <div className="box-border h-100 w-100 ">
+        <p>hi</p>
+        </div>
+
+
+        </>
+    )
+}
+
+export  function IngredientCard() {
+    return (
+        <>
+        <div className="box-border h-100 w-100 ">
+
+        </div>
+
+
+        </>
+    )
+
+}

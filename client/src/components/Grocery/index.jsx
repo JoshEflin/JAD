cd29d8a0d6c3298d46674aa4cd0d3dc8e2994cf2
ragingpenguin <eflinjh@gmail.com>
2023-05-03 16:54:24 -0400

    fixing a git error

--- client/src/components/Grocery/index.jsx
+++ client/src/components/Grocery/index.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-
-import Auth from '../../utils/auth';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+import { IngredientCard,RecipeCard } from './card';
+// import Auth from '../../utils/auth';
 
 const Grocery = () => {
     return (
         <div>
-            <h1>Placeholder</h1>
+            <h1 className = "bg-green">ingredient card template</h1>
+            < IngredientCard />
+
+            <h1>Recipe Card Template</h1>
+            <RecipeCard />
         </div>
     )
 }

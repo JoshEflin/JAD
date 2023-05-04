7b567b29500bd2adf04e993ef894bf4a44357d09
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-05-03 21:47:51 -0400

    added code to login user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import Home from './pages/Home'
 // import Signup from './pages/Signup'
-// import Login from './pages/Login'
+import Login from './pages/Login'
 // import Profile from './pages/Profile'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -50,8 +50,8 @@ function App() {
               {/* <Route
                 path='/login'
                 element={<Login />}
-              />
-              <Route
+              /> */}
+              {/* <Route
                 path='/signup'
                 element={<Signup />}
               />

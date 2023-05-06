1ba26012fbcaf914d2a6098dadac3663042ff9fd
Sander Weiss <weisssander@gmail.com>
2023-05-05 22:43:15 -0400

    profile should work well now

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import Avatar from 'react-avatar'
+import Avatar from 'react-avatar-edit'
 
 // import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
@@ -22,20 +22,25 @@ const Profile = () => {
         }
       }
     return (
-        <div>
+        <div className='text-center justify-center h-full my-10'>
             <h1>Profile</h1>
-            <Avatar
-                width={300}
-                height={300}
-                onCrop={onCrop}
-                onClose={onClose}
-                onBeforeFileLoad={onBeforeFileLoad}
-                src={null}
-            />
-            {preview && <img src={preview} alt="Preview" />}
-            <p>Username: </p>
-            <p>Email: </p>
-            <p></p>
+            <p>Avatar:</p>
+            <div className='inline-block'>
+                <Avatar
+                    width={200}
+                    height={200}
+                    onCrop={onCrop}
+                    onClose={onClose}
+                    onBeforeFileLoad={onBeforeFileLoad}
+                    src={null}
+                    className='block'
+                />
+            </div>
+                {/* {preview && <img src={preview} alt="Preview" />} */}
+            <p>Username:</p>
+            <p className='bg-gray-300 p-2 rounded-md text-lg inline-block'>Alexander1262</p>
+            <p>Email:</p>
+            <p className='bg-gray-300 p-2 rounded-md text-lg inline-block'>weisssander@gmail.com</p>
         </div>
     )
 }

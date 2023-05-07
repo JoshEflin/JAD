200871da2b29f2525e9941495a8f853a07670b3d
Sander Weiss <weisssander@gmail.com>
2023-05-07 11:27:37 -0400

    displaying icons

--- client/src/components/Homepage/index.jsx
+++ client/src/components/Homepage/index.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import recipeIcon from '../../assets/recipeicon.png'
+import groceryIcon from '../../assets/groceryicon.png'
 
 const Homepage = () => {
     return (
-        <div>
-            <h1>Check out our categories!</h1>
-
+        <div className='mt-10 mb-10'>
+            <h1 className=''>Check out our categories!</h1>
+            <div className='flex flex-row justify-center'>
+                <Link className='inline-block m-5' to=''>
+                    <img src={recipeIcon} className='w-40 h-40'/>
+                </Link>
+                <Link className='m-5'>
+                    <img src={groceryIcon} className='w-40 h-40'/>
+                </Link>
+            </div>
         </div>
     )
 }

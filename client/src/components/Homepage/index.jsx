677ad888f1f811aad1cba582319b4b54d486126b
Sander Weiss <weisssander@gmail.com>
2023-05-10 18:39:03 -0400

    fixed dans MASSIVE mistake

--- client/src/components/Homepage/index.jsx
+++ client/src/components/Homepage/index.jsx
@@ -33,8 +33,8 @@ function CouponSlider() {
         {
             breakpoint: 1024,
             settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             centerPadding: "40px",
             },
         },

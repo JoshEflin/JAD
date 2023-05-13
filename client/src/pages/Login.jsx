2130e5fce590b55532eace564c5262dc8e3cf379
ragingpenguin <eflinjh@gmail.com>
2023-05-13 12:00:08 -0400

    added some styles to log in

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
-
+import shop from "../assets/shop.png";
 import Auth from "../utils/auth";
 
 const Login = (props) => {
@@ -40,11 +40,7 @@ const Login = (props) => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
+        <img className="mx-auto h-10 w-auto" src={shop} alt="JAD Grocery" />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Login
         </h2>
@@ -77,7 +73,7 @@ const Login = (props) => {
             </div> */}
             <div className="relative">
               <input
-                className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600 rounded-lg"
+                className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600 rounded-lg"
                 placeholder=""
                 name="email"
                 type="email"
@@ -87,38 +83,32 @@ const Login = (props) => {
                 id="email"
                 required
               />
-            <label
+              <label
                 htmlFor="email"
                 className=" absolute left-0 top-2.5 px-2 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-5 peer-focus:text-gray-600 peer-focus:text-base"
               >
                 Email address
               </label>
 
-            <p className=" mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-              {" "}
-              Please enter a valid email address
-            </p>
+              <p className=" mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                {" "}
+                Please enter a valid email address
+              </p>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
+                {/* <div className="text-sm">
                   <a
                     href="#"
                     className="font-semibold text-indigo-600 hover:text-indigo-500"
                   >
                     Forgot password?
                   </a>
-                </div>
+                </div> */}
               </div>
 
-              <div className="mt-2">
+              <div className=" relative mt-2">
                 <input
                   placeholder="  ******"
                   name="password"
@@ -127,14 +117,20 @@ const Login = (props) => {
                   onChange={handleChange}
                   autoComplete="current-password"
                   required
-                  className="form-input px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className=" peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600 rounded-lg"
                 />
+                <label
+                  htmlFor="password"
+                  className=" absolute left-0 top-2.5 px-2 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-5 peer-focus:text-gray-600 peer-focus:text-base"
+                >
+                  Password
+                </label>
               </div>
             </div>
 
             <div>
               <button
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-green-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                 style={{ cursor: "pointer" }}
                 type="submit"
               >
